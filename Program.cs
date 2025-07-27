fa396f7f050f5f48148ee5abe6f89ef67a2efb3d
DestinyObs <destinyobueh14@gmail.com>
2025-07-27 02:55:21 +0100

    fixing errors

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
+// Register AppDbContext with SQL Server
+builder.Services.AddDbContext<CyberCloudDriveAPI.Data.AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 builder.Services.AddControllers();
@@ -10,6 +14,13 @@ builder.Services.AddIdentity<CyberCloudDriveAPI.Models.User, Microsoft.AspNetCor
     .AddEntityFrameworkStores<CyberCloudDriveAPI.Data.AppDbContext>();
 
 var app = builder.Build();
+// Seed initial data
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<CyberCloudDriveAPI.Data.AppDbContext>();
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<CyberCloudDriveAPI.Models.User>>();
+    CyberCloudDriveAPI.Data.SeedData.InitializeAsync(context, userManager).GetAwaiter().GetResult();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
